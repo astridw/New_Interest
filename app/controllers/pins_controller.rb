c36305696f0d09268b45f67394bc21ebc32422b4
Astrid <astridwcountee@gmail.com>
2015-09-12 13:23:04 -0500

    updated to add pictures

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -42,7 +42,7 @@ class PinsController < ApplicationController
      def set_pin
        @pin = Pin.find(params[:id])
      end
-  
+
 
     def correct_user
       @pin = current_user.pins.find_by(id: parmas[:id])
@@ -51,6 +51,6 @@ class PinsController < ApplicationController
 
      # Never trust parameters from the scary internet, only allow the white list through.
      def pin_params
-       params.require(:pin).permit(:description)
+       params.require(:pin).permit(:description, :image)
      end
  end
