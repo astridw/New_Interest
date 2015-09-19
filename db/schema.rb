cba6997cff92b52e39a60d25ec57117c22ffca90
Astrid <astridwcountee@gmail.com>
2015-09-19 14:13:28 -0500

    updated registration fields and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912175851) do
+ActiveRecord::Schema.define(version: 20150919190330) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -39,9 +39,12 @@ ActiveRecord::Schema.define(version: 20150912175851) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
